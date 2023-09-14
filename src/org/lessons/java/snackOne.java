953a7ad071e44c3b4c5a9c0cc9c05bcc5fbb404f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-14 11:37:07 +0200

    snack one

--- src/org/lessons/java/snackOne.java
+++ src/org/lessons/java/snackOne.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class snackOne {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Insert a number: ");
+		int userNumber = sc.nextInt();
+		
+		if(userNumber%2 == 0) {
+			System.out.println(userNumber);
+		} else {
+			System.out.println(userNumber + 1);
+		}
+		sc.close();
 	}
 }
