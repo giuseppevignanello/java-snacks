cab107705e2e77d22ee3ebba671ec41ce83fcca2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-14 14:08:10 +0200

    fix bug first input

--- src/org/lessons/java/snackFive.java
+++ src/org/lessons/java/snackFive.java
@@ -6,18 +6,21 @@ public class snackFive {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Insert a string to check how many alphabetic characters, how many numbers and how many symbols it contains. Or write 0 to stop the program ");
-		String userInput = sc.nextLine();
+		boolean blockProgram = false;
 		
 		int alphabeticalCharacters = 0;
 		int numberCharacters = 0; 
 		int symbolCharacters = 0;
 		
-		while (!userInput.equals("0")) {
+		while (!blockProgram) {
 			System.out.println("Insert a string");
-			userInput = sc.nextLine();	
+			String userInput = sc.nextLine();
+			
+			if( userInput.equals("0")) { 
+				blockProgram = true;
+			} else {
 			
 			char[] inputAsArray = userInput.toCharArray();
-			System.out.println(inputAsArray);
 			
 			for (int i = 0; i < inputAsArray.length; i++) {
 				if(Character.isLetter(inputAsArray[i])) {
@@ -29,10 +32,11 @@ public class snackFive {
 				}
 			}
 			
-			System.out.println("You have " + alphabeticalCharacters + " alpabetical characters\n" + numberCharacters + " number characters\n" + symbolCharacters + " symbol characters"  );
+			System.out.println("You have\n" + alphabeticalCharacters + " alpabetical characters\n" + numberCharacters + " number characters\n" + symbolCharacters + " symbol characters"  );
 			alphabeticalCharacters = 0;
 			numberCharacters = 0; 
 			symbolCharacters = 0;
+			}
 		}
 		
 		System.out.println("Bye");
