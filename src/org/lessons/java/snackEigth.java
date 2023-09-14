2dbb318b40d779af879f3ae5db7a63a3cbc592fb
giuseppevignanello <peppe.vignanello@gmail.com>
2023-09-14 15:39:53 +0200

    add min and max value

--- src/org/lessons/java/snackEigth.java
+++ src/org/lessons/java/snackEigth.java
@@ -7,15 +7,25 @@ public class snackEigth {
 	public static void main(String[] args) {
 		
 		int[] rndNumbersArray = new int [10]; 
+		int minValue = 150;
+		int maxValue = 0; 
 		
 		for (int i = 0; i < rndNumbersArray.length; i++) {
 			Random rnd = new Random();
 			int rndNumber = rnd.nextInt(150 - 100) + 100; 
 			
 			rndNumbersArray[i] = rndNumber;
-		}
+			if (rndNumbersArray[i] < minValue) {
+				minValue = rndNumbersArray[i];
+			}
+			if (rndNumbersArray[i] > maxValue) {
+				maxValue = rndNumbersArray[i];
+			}
+		};
 		
-		System.out.println(Arrays.toString(rndNumbersArray));
+		
+		
+		System.out.println("Random numbers: " + Arrays.toString(rndNumbersArray) + "\n" + "Min value: " + minValue + "\n" + "Max value: " + maxValue);
 		
 	}
 }
